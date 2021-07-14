e27953cdf5588d9544ffb6f0668c995830a79a25
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-07-14 10:50:04 +0200

    update dep

--- Package.swift
+++ Package.swift
@@ -7,13 +7,9 @@ let package = Package(
   name: "alfred-soulvercore",
   platforms: [.macOS(.v10_15)],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/soulverteam/SoulverCore", from: "1.3.1")
+    .package(url: "https://github.com/soulverteam/SoulverCore", from: "1.4.0")
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "alfred-soulvercore",
       dependencies: ["SoulverCore"])
