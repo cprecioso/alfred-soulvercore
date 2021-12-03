35fb4b97d548c2a6306e8b2e29435ccd7a9275fb
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-12-03 16:12:33 +0100

    Fix rpath in built executable

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,5 @@ swift build -c release --arch arm64 --arch x86_64
 rm -rf .build/release
 mkdir -p .build/release
 cp -R Resources/* .build/apple/Products/Release/alfred-soulvercore .build/apple/Products/Release/*.framework .build/release
+install_name_tool -rpath '@executable_path/../lib' '@executable_path' .build/release/alfred-soulvercore
 cd .build/release && zip -r ../SoulverCore.alfredworkflow ./*
